9f6b5b5cc81040baca4d40aab51fb9dce246ac67
Tim Newton <timothy.newton.cma13@gmail.com>
2019-12-28 12:49:30 -0500

    massive rebranding and readme update

--- atop/options/putoption.py
+++ atop/options/putoption.py
@@ -1,4 +1,4 @@
-from allthingsoptions.options.binomialoption import BinomialOption
+from atop.options.binomialoption import BinomialOption
 
 class PutOption(BinomialOption):
     def __init__(self, stock_price, strike_price, up_price, down_price, risk_free, override_up_payoff = None, override_down_payoff = None, override = False):

3c6d440c79ee643c8e7fe3fb5dc88e07aff14ff6
Timothy Newton <timothy.newton.cma13@gmail.com>
2019-12-30 09:18:11 -0500

    add more markdown, add new class

---
+++ atop/options/binodes.py
@@ -0,0 +1,12 @@
+# notes to self.
+# every node has 2 child nodes or 2 payoff_nodes
+# assume states don't reconnect for now.
+# 
+
+
+class Binodes:
+    def __init__(self, u_node, d_node):
+        pass
+
+
+# going to need to describe this all on paper and then program.

1c7a698b94ff1ee8afe6fe5e8e512d4f3f2e0398
Tim Newton <timothy.newton.cma13@gmail.com>
2020-01-05 12:02:50 -0500

    start on blackscholes and nodes

--- atop/options/binomialoption.py
+++ atop/options/binomialoption.py
@@ -12,7 +12,9 @@ class BinomialOption:
         User can override these calculated payoffs by passing their own values.
         This is useful for calculating an 'inner' single period of a much larger multi-period option pricing model.
         Consider that the payoffs are not the possible stock values at some arbitrary time before maturity but 
-        instead the price of the next binomial period option. 
+        instead the price of the next binomial period option.
+
+        Dev note: This easily causes confusion, consider breaking apart this functionality.
         '''
     def __init__(self, stock_price, strike_price, up_price, down_price, risk_free, up_payoff, down_payoff, overridden=False):
         self.stock_price = stock_price

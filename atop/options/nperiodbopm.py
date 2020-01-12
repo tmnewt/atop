75b4d0aed17a14c3dca088ab8155fcb9a37ba776
Tim Newton <timothy.newton.cma13@gmail.com>
2020-01-12 12:31:56 -0500

    add trade position

--- atop/options/nperiodbopm.py
+++ atop/options/nperiodbopm.py
@@ -1,10 +1,11 @@
 class NPeriodBOPM:
 
-    def __init__(self, underlying, strike, volatility, risk_free, nperiods):
+    def __init__(self, underlying, strike, volatility, risk_free, nperiods, trade_position):
         self.underlying = underlying
         self.strike = strike
         self.volatility = volatility
         self.risk_free = risk_free
         self.nperiods = nperiods
+        self.trade_position = trade_position
 
         print(NotImplemented)

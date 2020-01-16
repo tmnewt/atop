0ee28b0ffff603dc59c79202c9ff706087c9a43e
Tim Newton <timothy.newton.cma13@gmail.com>
2020-01-16 16:51:09 -0500

    I hate print statements

--- atop/teaching/singleperiod.py
+++ atop/teaching/singleperiod.py
@@ -1,4 +1,5 @@
 from math import sqrt, log, exp
+import pprint
 
 class SinglePeriodOption:
     '''Single Period Binomial Option object
@@ -123,9 +124,10 @@ class SinglePeriodOption:
     
 
     # generates a complete solution with choices to display certain information.
-    def solution(self, rounding = 2, skip_problem_display = False,
+    def solution(self, skip_problem_display = False,
                 hide_additional = False):
         
+        # items that appear numerous times.
         pos=          self.position
         opt=          self.optype
         name=         self.underlying_name
@@ -137,9 +139,9 @@ class SinglePeriodOption:
         
 
         # message about input values
-        print(f'object input: SinglePeriodOption({pos}, {opt}, {under}, {strike}, {up}, {down}, {rf}, {name})')
+        print(f'argument input: SinglePeriodOption({pos}, {opt}, {under}, {strike}, {up}, {down}, {rf}, {name})')
         if skip_problem_display:
-            print(f'{pos} {opt} value: $ {self.value}')
+            print(f'{pos} {opt} value: $ {round(self.value,4)}')
             
         else:
             # Some visual padding
@@ -158,55 +160,85 @@ class SinglePeriodOption:
         
         # Still in the print_calc_values function
             print('-------------------')
-            print(f'Answer: {pos} {opt} value: $ {self.value}')
+            print(f'Answer: {pos} {opt} value: $ {round(self.value,4)}')
             print('-------------------')
             
 
             if hide_additional:
                 pass
             else:
-                print('Problem\'s up factor: {}'.format(self.__up_factor()))
-                print('Problem\'s down factor: {}'.format(self.__dn_factor()))
-                print('Hedge Ratio (a.k.a units of underlying): {}'.format(
-                        round(self.hedge_ratio, rounding)))
-                print('Value of bond position for hedging purposes: $ {}'.format(
-                        round(self.rf_units, rounding)))
-                print('Risk-neutral probability for up state is {}'.format(
-                        round(self.up_risk_neutral_prob, rounding)))
-                print('Risk-neutral probability for down state is {}'.format(
-                        round(self.down_risk_neutral_prob, rounding)))
+                print('Intermediate calculation answers:')
+                print(f'Problem\'s up factor: {round(self.__up_factor(),4)}')
+                print(f'Problem\'s down factor: {round(self.__up_factor(),4)}')
+                print(f'Hedge Ratio (a.k.a units of underlying): {round(self.hedge_ratio,4)}')
+                print(f'Value of bond position for hedging purposes: $ {round(self.rf_units,4)}')
+                print(f'Risk-neutral probability for up state is {round(self.up_risk_neutral_prob,4)}')
+                print(f'Risk-neutral probability for down state is {round(self.down_risk_neutral_prob,4)}')
         print('____________________________________________________________\n')
 
             
-            
-    def guide(self):
-        print('''Remember: all options are contractual agreements between two parties who are legally bound
-to perform as specified by their agreement. So think of the word \'Option\' as the same thing as
-'contract'. Further, these contracts are standardized with universal well defined and understood 
-clauses and language which allows the agreements to be easily transferred to others (third-parties) 
-without loss of contractual duty or value. Lastly, remember all options derive their value from the 
-performance of the underlying asset over the life of the contract.
 
-The two parties to these contracts are a BUYER (a.k.a going LONG) of the contract who pays a price 
-(a.k.a. a premium) and on the other side is the party WRITING the option (a.k.a the SELLER, a.k.a shorting) 
-who collects the premium the buyer paid. The contract is formed with the buyer purchasing certain 
-contractual rights for which the seller is contractually obligated to fullfill.
 
-As you will see this contract describes a zero-sum transaction. The gains of one party are the losses of the 
-other party. 
 
-One note:
-This guide uses the terms \'price\' and \'value\' interchangablely. They are the same thing. 
-Additionally, you might see the program produce negative currency values. For instance,
-you may see the calculated value for a short call be, say, -2.50. Which seems weird. 
-How can a price be negative? This is simply an accounting style chosen to give logical 
-consistency to the transactions. This is important for correctly describing replicating 
-portfolios. It is centered around the perspective of the buyer who pays a physical x 
-amount of money, thus a positive cash outflow. Conversely the seller earns the premium x
-in the form of a negative outflow (which translates to a cash inflow). It's not an actual negative value. 
-    ''')
-        print('Recall that this a {} option'.format(self.optype))
-        self.__print_helper()
+
+    def guide(self, hide_intro = False):
+        
+        pos=          self.position
+        opt=          self.optype
+        name=         self.underlying_name
+        under=  round(self.underlying_value, 2)
+        strike= round(self.strike_value, 2)
+        up=     round(self.up_value, 2)
+        down=   round(self.down_value, 2)
+        rf=     round(self.risk_free, 2)
+        
+        
+        if hide_intro:
+            pass
+        
+        # suggestion to collapse all this in your IDE
+        else:
+            print(self.__intro_guide())
+
+        print('**************************************************')
+        print(f'  Start of Walkthrough for {pos} {opt} on {name}')
+        print('**************************************************')
+
+
+        if self.position == 'Long':
+            print(f'Remember: going long simply means purchasing the option on {name}.')
+            if self.optype == 'Call':
+                print(f'''\nBuying a Call gives you the RIGHT but NO OBLIGATION to buy the underlying
+asset ({name}) for the strike price of ${strike}. ({name}'s value is currently trading at ${under})
+
+These facts are important for 2 reasons for a long call contract:
+First, because this contract gives you the RIGHT to purchase {name} at ${strike}, when
+{name} moves higher than the strike price of ${strike} then the value of this call 
+option (that is, its price) increases. So, when {name}\'s price > ${strike} 
+then 
+this contract becomes more valuable! If contract matures with {name}\'s price > ${strike}
+then the value of the contract is the difference in {name}\'s price - strike.
+
+Second, because this contract requires NO OBLIGATION on your part to buy {name} at ${strike}, 
+if {name}\'s price remains lower than the strike price of ${strike} then you 
+are free to walk away. So, while {name}\'s price < ${strike} then this contract 
+becomes worth less and less. If 
+    
+These feature are what makes call options special.
+What this all means is there is potential for huge upside if {name}\'s is higher than''')
+
+            else:
+                print('''   Buying a put gives you the RIGHT to SELL the underlying asset at
+    the strike price. But you have NO OBLIGATION to exercise 
+    this right if the underlying > strike. This means you have 
+    unlimited upside and limited downside.''')
+
+        #else:
+        #    print('Shorting means selling the option (a.k.a writing an option)')
+
+
+
+        
         
         print('First, find the payoffs in the up and down state:')
         print('For the up payoff: up_value - underlying')
@@ -234,40 +266,41 @@ in the form of a negative outflow (which translates to a cash inflow). It's not 
         print('Dn factor: {}'.format(self.__sanity_check(self.volatility[3])[1]))
         print('____________________________________________________________\n')
 
-    def __print_helper(self): #helper func for guide()
-        if self.optype == 'Long':
-            print('Going long means purchasing the {} option'.format(
-                self.position))
-            if self.optype == 'Call':
-                print('''   Buying a call gives you the RIGHT but NO OBLIGATION to PURCHASE the underlying
-    asset {name} FOR the strike price of ${strike} ({name}'s value is currently trading at ${under})
 
-    These facts are important for 2 reasons for a long call contract:
-    First, because this contract gives you the RIGHT to purchase {name} at ${strike}, if 
-    {name} moves higher than the strike price of ${strike} then the value of this call 
-    option (that is, its price) increases. So, when {name}\'s price > ${strike} 
-    then this contract becomes more valuable! If contract matures with {name}\'s price > ${strike}
-    then the value of the contract is the difference in {name}\'s price - strike.
+    def __intro_guide(self):
+        intro_text = '''Remember: all options are contractual agreements between two parties who are legally bound
+to perform as specified by their agreement. So think of the word `Option` as the same thing as
+`contract`. Further, these contracts are standardized with universal well defined and understood 
+clauses and language which allows the agreements to be easily transferred to others (third-parties) 
+without loss of contractual duty or value. Lastly, remember all options derive their value from the 
+performance of the underlying asset over the life of the contract.
 
-    Second, because this contract requires NO OBLIGATION on your part to buy {name} at ${strike}, 
-    if {name}\'s price remains lower than the strike price of ${strike} then you 
-    are free to walk away. So, while {name}\'s price < ${strike} then this contract 
-    becomes worth less and less. If 
-    
-    These feature are what makes call options special.
-    What this all means is there is potential for huge upside if {name}\'s is higher than
-    ''')
+The two parties to these contracts are a BUYER (a.k.a going LONG) of the contract who pays a price 
+(a.k.a. a premium) and on the other side is the party WRITING the option (a.k.a the SELLER, a.k.a shorting) 
+who collects the premium the buyer paid. The contract is formed with the buyer purchasing certain 
+contractual rights for which the seller is contractually obligated to fullfill.
 
-            else:
-                print('''   Buying a put gives you the RIGHT to SELL the underlying asset at
-    the strike price. But you have NO OBLIGATION to exercise 
-    this right if the underlying > strike. This means you have 
-    unlimited upside and limited downside.''')
-
-        else:
-            print('Shorting means selling the option (a.k.a writing an option)')
+As you will see this contract describes a zero-sum transaction. The gains of one party are the losses of the 
+other party. 
 
     
+Lastly, this guide uses the terms `price` and `value` interchangablely. They are the same thing. 
+Additionally, you might see the program produce negative currency values. For instance,
+you may see the calculated value for a short call be, say, -2.50. Which seems weird. 
+How can a price be negative? This is simply an accounting style chosen to give logical 
+consistency to the transactions. This is important for correctly describing replicating 
+portfolios. It is centered around the perspective of the buyer who pays a physical x 
+amount of money, thus a positive cash outflow. Conversely the seller earns the premium x
+in the form of a negative outflow (which translates to a cash inflow).'''
+        #pprint.pprint(intro_text)
+        #print(type(intro_text))
+        #test = intro_text.split('\n')
+        #print(test)
+        #temp_string = ''.join(test)
+        #print(temp_string)
+        #print(type(temp_string))
+        print(f'{intro_text:<80}')
+        #return intro_text
 
 #test   
 #example = SinglePeriodOption('Long','Call', 100, 110, 120, 90.25, 0.05)
@@ -281,6 +314,6 @@ in the form of a negative outflow (which translates to a cash inflow). It's not 
 # cool, it works.
 
 #additional tests.
-example = SinglePeriodOption('Short','Call', 95, 110, 120, 90.25, 0.05)
-example.solution()
+example = SinglePeriodOption('Long','Call', 95.54, 107.89, 120.02, 90.25, 0.0342, 'TMNQQC')
+example.guide()
     

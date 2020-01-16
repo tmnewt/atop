d4f43ef127b91da15d6c5f26e60d742216b6d551
Tim Newton <timothy.newton.cma13@gmail.com>
2020-01-16 17:59:17 -0500

    kill old files

--- atop/teaching/simpleput.py
+++
@@ -1,9 +0,0 @@
-from atop.simpleops.simpleoption import SimpleOption
-
-class SimplePut(SimpleOption):
-    def __init__(self, stock_price, strike, up_price, down_price, risk_free):
-        
-        up_payoff = max(strike - up_price, 0)
-        down_payoff = max(strike - down_price, 0)
-
-        super().__init__(stock_price, strike, up_price, down_price, up_payoff, down_payoff, risk_free)

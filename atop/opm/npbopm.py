a1522389e344732ea8aeb5d8dc9af7d4726ac3ae
Tim Newton <timothy.newton.cma13@gmail.com>
2020-01-19 15:20:55 -0500

    bring class inline with standards

--- atop/opm/npbopm.py
+++ atop/opm/npbopm.py
@@ -4,23 +4,22 @@ import numpy as np
 
 class NPeriodBOPM:
 
-    def __init__(self, op_type, 
-                        underlying, strike, 
-                        volatility, risk_free, 
-                        nperiods, time_in_years, 
-                        factor_method = 'Jarrow',
-                        trade_position = 'Long' 
-                        ):
-        
+    def __init__(self, position: str, op_type: str, 
+                    underlying_value_value: float or int, 
+                    strike_value: float or int, 
+                    volatility: float, risk_free: float, 
+                    nperiods: int, time_in_years: float or int, 
+                    factor_method = 'Jarrow'):
+        self.position = position
         self.op_type = op_type
-        self.underlying = underlying
-        self.strike = strike
+        self.underlying_value = underlying_value_value
+        self.strike_value = strike_value
         self.volatility = volatility
         self.risk_free = risk_free
         self.nperiods = nperiods
         self.time_in_years = time_in_years
         self.factor_method = factor_method
-        self.trade_position = trade_position
+        
 
         # internal calculations
         self.deltatime = self.__deltatime_calc()
@@ -39,14 +38,14 @@ class NPeriodBOPM:
             self.up_neutral = self.__cox_calc()[2]
             self.down_neutral = self.__cox_calc()[3]
 
-        self.underlying_vector = self.__underlying_vector_calc()
+        self.underlying_value_vector = self.__underlying_value_vector_calc()
         self.payoff_vector = self.__payoff_vector_calc()
         self.price_vector = self.__price_calc()
-        self.price = self.price_vector[0]
+        self.value = self.price_vector[0]
 
     # internal calc
 
-    def __deltatime_calc(self):
+    def __deltatime_calc(self) -> float:
         return self.time_in_years / self.nperiods
 
     def __jarrow_calc(self):   
@@ -80,21 +79,21 @@ class NPeriodBOPM:
 
         return (up_factor, dn_factor, up_neutral, dn_neutral)
 
-    def __underlying_vector_calc(self):
+    def __underlying_value_vector_calc(self):
         # Special thanks to cantaro86 for posting his solution on github
-        underlying_values = np.array(
-            [(self.underlying * self.upfactor**j * 
+        underlying_vector = np.array(
+            [(self.underlying_value * self.upfactor**j * 
             self.downfactor**(self.nperiods-j)) 
             for j in range(self.nperiods + 1)])
-        return underlying_values
+        return underlying_vector
 
     def __payoff_vector_calc(self):
         # Special thanks to cantaro86 for posting his solution on github
         payoffs = np.zeros(self.nperiods + 1) # get array going
         if self.op_type == 'Call':
-            payoffs[:] = np.maximum(self.underlying_vector - self.strike, 0.0)
+            payoffs[:] = np.maximum(self.underlying_value_vector - self.strike_value, 0.0)
         else:
-            payoffs[:] = np.maximum(self.strike - self.underlying_vector, 0.0)
+            payoffs[:] = np.maximum(self.strike_value - self.underlying_value_vector, 0.0)
         return payoffs
 
     def __price_calc(self):
@@ -108,8 +107,8 @@ class NPeriodBOPM:
                     + self.down_neutral * price_vector[:-1]))
         return price_vector
         
-    def get_price(self):
-        return self.price
+    def get_value(self):
+        return self.value
 
         
 #test:
@@ -117,6 +116,6 @@ class NPeriodBOPM:
 #print(example.deltatime)
 #print(example.upfactor)
 #print(example.downfactor)
-#print(example.underlying_vector)
+#print(example.underlying_value_vector)
 #print(example.price_vector)
 #print(example.price)

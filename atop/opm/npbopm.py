1365396a3721b134159bb53a0b9980408b737421
Tim Newton <timothy.newton.cma13@gmail.com>
2020-01-20 22:59:25 -0500

    add 'step-in' method for nperiod as demonstration

--- atop/opm/npbopm.py
+++ atop/opm/npbopm.py
@@ -56,8 +56,8 @@ class NPeriodBOPM:
 
         self.underlying_value_vector = self.__underlying_value_vector_calc()
         self.payoff_vector = self.__payoff_vector_calc()
-        self.price_vector = self.__price_calc()
-        self.value = self.price_vector[0]
+        self.value_vector = self.__value_calc()
+        self.value = self.value_vector[0]
 
     # internal calc
 
@@ -112,16 +112,59 @@ class NPeriodBOPM:
             payoffs[:] = np.maximum(self.strike_value - self.underlying_value_vector, 0.0)
         return payoffs
 
-    def __price_calc(self):
+    def __value_calc(self):
         # Special thanks to cantaro86 for posting his solution on github
-        price_vector = self.payoff_vector
-        # find prices
+        value_vector = self.payoff_vector
+        # find values
         for i in range(self.nperiods-1, -1, -1):
-            price_vector[:-1] = (
+            value_vector[:-1] = (
                 np.exp(-self.risk_free*self.deltatime)
-                * (self.up_neutral * price_vector[1:]
-                    + self.down_neutral * price_vector[:-1]))
-        return price_vector
+                * (self.up_neutral * value_vector[1:]
+                    + self.down_neutral * value_vector[:-1]))
+        return value_vector
+
+    def value_calc_step_in(self):
+        # Special thanks to cantaro86 for posting his solution on github
+        
+        # find values
+        if self.nperiods > 10:
+            print('refuse to run more than 9 iterations for the step-in example!')
+        else:
+            underlying_step_in = np.array(
+                [(self.underlying_value * self.upfactor**j * 
+                self.downfactor**(self.nperiods-j)) 
+                for j in range(self.nperiods + 1)])
+            print('possible underlying end states:')
+            print(underlying_step_in)
+
+            print('calculating payoffs')
+            payoffs_step_in = np.zeros(self.nperiods + 1) # get array going
+            if self.optype == 'Call':
+                payoffs_step_in[:] = np.maximum(underlying_step_in - self.strike_value, 0.0)
+            else:
+                payoffs_step_in[:] = np.maximum(self.strike_value - underlying_step_in, 0.0)
+            print(payoffs_step_in)
+            value_vector_step_in = payoffs_step_in
+            
+            print('copy payoff vector to value_vector')
+
+            for i in range(self.nperiods-1, -1, -1):
+                print('\nNEXT ITERATION')
+                print('value_vector at start')
+                print(value_vector_step_in)
+                print(f'step 1: multiply last {self.nperiods} values in value vector by up_risk_neutral scalar')
+                print(f'step 2: multiply first {self.nperiods} value in value vector by down_rik_neutral scalar')
+                print('step 3: add product vectors together.')
+                print('step 4: discount resulting vector by e^(-risk-free * deltatime)')
+                value_vector_step_in[:-1] = (
+                    np.exp(-self.risk_free*self.deltatime)
+                    * (self.up_neutral * value_vector_step_in[1:]
+                        + self.down_neutral * value_vector_step_in[:-1]))
+                print('value_vector at end of iteration')
+                print(value_vector_step_in)
+            print('task completed')
+            print(f'option value is at zero index: {value_vector_step_in[0]}')
+        return
         
     def get_value(self):
         return self.value
@@ -133,5 +176,5 @@ class NPeriodBOPM:
 #print(example.upfactor)
 #print(example.downfactor)
 #print(example.underlying_value_vector)
-#print(example.price_vector)
-#print(example.price)
+#print(example.value_vector)
+#print(example.value)
